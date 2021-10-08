f38ac477332cddeae157cebf69b2466c2ab8b731
ArianeBlow <61753065+ArianeBlow@users.noreply.github.com>
2021-10-08 16:51:15 +0200

    Update port-scan.sh

--- port-scan.sh
+++ port-scan.sh
@@ -42,7 +42,7 @@ echo ""
 echo "[-] you must provide a vlan as first argument and the time sleep time between every request in the second arg."
 echo "    Exemple : script.sh 192.168.1 9"
 echo ""
-echo "    Ports can be modified in line 75"
+echo "    Ports can be modified in line 81"
 echo ""
 exit
 }
